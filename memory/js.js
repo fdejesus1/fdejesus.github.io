293450d8ecdcd3d5dd0874631f311ffcc87708c4
fdejesus1 <fdejesus@kippnyccp.org>
2015-03-10 08:57:34 -0400

    Update js.js

--- memory/js.js
+++ memory/js.js
@@ -6,5 +6,13 @@ $(document).ready(function(){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
   var click_counter=0;
+    $(".card").click(function(){
+    if(click_counter==0){
+      click_counter == 1;
+    	$(this).find("p").css("opacity", "1");}
+    else{
+      
+    }
 
+});
 });
