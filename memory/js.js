ae1a44ad45476698e6e779abb35bf60cf388b69b
fdejesus1 <fdejesus@kippnyccp.org>
2015-03-11 09:34:24 -0400

    Update js.js

--- memory/js.js
+++ memory/js.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
   var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
-
+  var thecard;
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
@@ -9,7 +9,8 @@ $(document).ready(function(){
     $(".card").click(function(){
     if(click_counter==0){
       click_counter = 1;
-    	$(this).find("p").css("opacity", "1"); 
+      thecard = $(this).find("p");
+    	thecard.css("opacity", "1"); 
     	var first_click = $(this).find("p").html();
       
     }
@@ -18,7 +19,9 @@ $(document).ready(function(){
       
     }
     $(".card").click(function(){
-      if(first_click==second_click).css("opacity", "0")
+      if(first_click==second_click){
+      thecard .css("opacity", "0")
     
+}
 });
 });
