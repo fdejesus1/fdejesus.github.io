29b9dface85fc107808f6951d13a96dc44841a8b
fdejesus1 <fdejesus@kippnyccp.org>
2015-03-20 08:46:56 -0400

    Update js.js

--- shopping-list/js.js
+++ shopping-list/js.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-  $('button').on('click',function(){
-    var val1 = $('input').val();
-    $('#mydiv').append("<p>"+val1+"</p>");
-    console.log(val1);
+  $('#add').on('click',function(){//when the button is clicked....
+    var val1 = $('input').val();//sets var val1 equal to the user input
+    $('#mydiv').append("<p>"+val1+"</p>");//adding html to the the end of the string and makes a list 
+    console.log(val1);//checks if the code is working 
+  $('#clear-button').on('click',function(){
+  $('#mydiv').empty();
+  });
   });
 })
 
