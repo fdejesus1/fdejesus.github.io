0e1621d83595b40db40949e261c7cd62975dcf66
fdejesus1 <fdejesus@kippnyccp.org>
2015-05-01 09:08:25 -0400

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,50 @@
+def rolldice(number):
+    two = 0
+    three = 0
+    four = 0
+    five = 0
+    six = 0
+    seven = 0
+    eight = 0
+    nine = 0
+    ten= 0
+    eleven = 0
+    twelve = 0
+    for i in range(500):
+      roll=random.radiant(1,6)+random.randiant(1,6)
+      
+      if roll == 2
+        two = two +1
+      if roll == 3
+        three = three+1
+      if roll == 4
+        four = four+1
+      if roll == 5
+        five = five+1
+      if roll == 6
+        six = six+1
+      if roll == 7
+       seven = seven+1
+      if roll == 8
+       eight = eight+1
+      if roll == 9
+       nine = nine+1
+      if roll == 10
+       ten = ten+1
+      if roll == 11
+       eleven = eleven+1
+      if roll == 12
+       twelve = twelve+1 
+
+print two / 500
+print three / 500
+print four /500 
+print five / 500
+print six /500
+print seven / 500
+print eight /500
+print nine/500
+print ten /500
+print eleven/500
+print twelve/500
+  
