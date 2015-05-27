7133c6aed40ab39ad242c73bc490b043bb7da025
fdejesus1 <fdejesus@kippnyccp.org>
2015-05-27 09:30:17 -0400

    Create obsticle_avoider.py

---
+++ obsticle_avoider.py
@@ -0,0 +1,31 @@
+from gopigo import *
+
+
+
+def move_forward(feet)
+  mm= feet * 304.8
+  steps=mm / 11.34
+  steps= int(math.floor(steps))
+    enc_tgt(1,1,steps)
+    fwd()
+    time.sleep(feet*2)
+
+def turn_right()
+    enc_tgt(1,0,18)
+    time.sleep(.1)
+    right()
+    time.sleep(2)
+
+def turn_left()
+    enc_tgt(0,1,18)
+    time.sleep(.1)
+    left()
+    time.sleep(2)
+while True 
+while us_dist(15) > 15:
+  fwd()
+turn_right()
+
+us_dist(15) < 15
+print us_dist(15)
+distance_to 
